83e98ab30fde2aebb3742095a06cfd1921aefedb
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-02-18 03:39:17 -0300

    adicação do data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO tb_Pessoa (pessoa_id, nome, matricula, email, cpf, tipo_Usuario, telefone) 
-VALUES (1, 'Ana', '123456789', 'ana@gmail.com', '0987654321', 1, '92991921009');
+VALUES (1, 'Ana', '123456789', 'ana@gmail.com', '0987654321', 0, '92991921009');
+
+INSERT INTO tb_Pessoa (pessoa_id, nome, matricula, email, cpf, tipo_Usuario, telefone) 
+VALUES (2, 'Isaac', '0987654', 'isaac@gmail.com', '09876543213', 1, '92994819284');
+
+INSERT INTO tb_Pessoa (pessoa_id, nome, matricula, email, cpf, tipo_Usuario, telefone) 
+VALUES (3, 'Felipe', '0000000', 'felipe@gmail.com', '7738290433', 2, '9297856743');
